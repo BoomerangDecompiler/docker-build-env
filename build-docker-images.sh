21d773c1d41cd9b871d47cd291f4076c8c18300e
ceeac <ceeac@users.noreply.github.com>
2018-11-27 10:05:45 +0100

    Push docker images to Docker Hub

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -2,4 +2,9 @@
 
 for img in $(ls dockerfiles); do
     sudo docker build -t ceeac/boomerang-build-env:$img dockerfiles/$img/
+
+    if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+        echo $DOCKER_PASSWORD | sudo docker login -u $DOCKER_USERNAME --password-stdin
+        sudo docker push ceeac/boomerang-build-env:$img
+    fi
 done
